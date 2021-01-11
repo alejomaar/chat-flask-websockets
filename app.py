790302223030c7bec7a5b8f9092963d47ca0ac05
alejomar <demonmaar@gmail.com>
2021-01-11 14:14:14 -0500

    new evenlet

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask, render_template, make_response, redirect
-from flask_socketio import SocketIO, send, emit
-import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO,send, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app,logger=True, engineio_logger=True,cors_allowed_origin="*")
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@socketio.on('connect')
+def connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def disconnect():
+    print('Client disconnected')
 
 @socketio.on("message")
 def handleMessage(data):
     emit("new_message",data,broadcast=True)
     
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app)
     #, host='0.0.0.0', port=5004
